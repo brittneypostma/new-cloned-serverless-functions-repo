b62fc27c99b62959afeb77be39b6a2edb03fbbe6
brittneypostma <bdesignwebdev@gmail.com>
2022-07-19 23:09:56 -0400

    post event data to serverless function

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch'
 
-export const handler = async () => {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+export const handler = async (event, context) => {
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
   const response = await fetch(POKE_API)
   const data = await response.json()
@@ -9,7 +10,7 @@ export const handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data
+      pokemon: data.pokemon_entries
     })
   }
 }
